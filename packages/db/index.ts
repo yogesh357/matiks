e92469b8f88463967f78214b1bd04841cea9c5d7
yogesh357 <ypdhakane101@gmail.com>
2026-09-16 22:52:34 +0530

    added prisma config

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+// import "dotenv/config";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
